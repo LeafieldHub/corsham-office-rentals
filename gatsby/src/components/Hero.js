2f1950fbe340699c5bd7758f55c09adfc4ff237e
LeafieldHub <leafieldhub@gmail.com>
2026-01-16 16:53:54 +0000

    Final button swap: Only blue Contact button

--- gatsby/src/components/Hero.js
+++ gatsby/src/components/Hero.js
@@ -28,16 +28,8 @@ export default function Hero({ title, text, image }) {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <Link
-                    to="#availability"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-primary700 md:py-4 md:text-lg md:px-10"
-                  >
-                    Availability
-                  </Link>
-                </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <Link
                     to="/contact"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-50 hover:bg-blue-100 md:py-4 md:text-lg md:px-10"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-opacity-90 md:py-4 md:text-lg md:px-10"
                   >
                     Contact
                   </Link>
