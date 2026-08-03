bf70863e4fccbf73179c19e366a837e3dd102e9e
LeafieldHub <leafieldhub@gmail.com>
2026-08-03 17:57:44 +0200

    Update gatsby-config.js

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       options: {
         projectId: 'chergncb',
         dataset: 'production',
-        watchMode: true,
+        watchMode: false,
         token: process.env.SANITY_TOKEN,
       },
     },
