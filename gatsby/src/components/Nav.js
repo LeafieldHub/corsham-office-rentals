4dc2808cf263e3490332a79ae6d070c8fe4fe2c3
LeafieldHub <leafieldhub@gmail.com>
2026-08-04 17:20:31 +0200

    Update Nav.js

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { graphql, Link, StaticQuery } from 'gatsby';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import logoImg from '../images/logo transparency.png';
+import logoImg from '../images/logo white .png';
 
 export default function Navigation() {
   return (
