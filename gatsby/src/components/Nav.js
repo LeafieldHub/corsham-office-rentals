e0f1e6ab63742cd9d35674e5081bf2681548d774
LeafieldHub <leafieldhub@gmail.com>
2026-08-04 15:12:13 +0200

    Update Nav.js

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -32,7 +32,7 @@ const NavBar = ({ sanitySiteSettings }) => {
   const siteTitle = sanitySiteSettings?.title || "Corsham Office Rentals";
 
   return (
-    <nav className="bg-blue-600">
+    <nav className="bg-slate-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center w-full justify-between">
@@ -75,7 +75,7 @@ const NavBar = ({ sanitySiteSettings }) => {
             <button
               id="toggle"
               type="button"
-              className="bg-blue-900 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="bg-blue-700 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
               onClick={() => setOpen(!open)}
