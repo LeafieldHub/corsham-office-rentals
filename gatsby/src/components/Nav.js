47065ed651513d90874e053b88dd019cf368fefa
LeafieldHub <leafieldhub@gmail.com>
2026-08-04 14:53:33 +0200

    Update Nav.js

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -28,13 +28,12 @@ export default function Navigation() {
 const NavBar = ({ sanitySiteSettings: { menu } }) => {
   const [open, setOpen] = useState();
   return (
-    <nav className="bg-red-600">
+    <nav className="bg-blue-600">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
-                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                 <Link to="/" className="nav-item" activeClassName="active">
                   Home
                 </Link>
@@ -59,21 +58,15 @@ const NavBar = ({ sanitySiteSettings: { menu } }) => {
           </div>
 
           <div className="-mr-2 flex md:hidden">
-            {/* <!-- Mobile menu button --> */}
             <button
               id="toggle"
               type="button"
-              className="bg-red-900 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              className="bg-blue-900 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
               onClick={() => setOpen(!open)}
             >
               <span className="sr-only">Open main menu</span>
-              {/* <!--
-              Heroicon name: outline/menu
-
-              Menu open: "hidden", Menu closed: "block"
-            --> */}
               <svg
                 className="block h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
@@ -89,11 +82,6 @@ const NavBar = ({ sanitySiteSettings: { menu } }) => {
                   d="M4 6h16M4 12h16M4 18h16"
                 />
               </svg>
-              {/* <!--
-              Heroicon name: outline/x
-
-              Menu open: "block", Menu closed: "hidden"
-            --> */}
               <svg
                 className="hidden h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
@@ -114,10 +102,8 @@ const NavBar = ({ sanitySiteSettings: { menu } }) => {
         </div>
       </div>
 
-      {/* <!-- Mobile menu, show/hide based on menu state. --> */}
       <div className={open ? 'block' : 'hidden'} id="mobile-menu">
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
           <Link
             onClick={() => setOpen(!open)}
             to="/"
